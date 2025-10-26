b492b253f86314ab44bbdfb8f15c0087a9157cd7
mwiti-fidelis <f.mwiti@alustudent.com>
2025-10-26 16:52:01 +0200

    corrected errors in files

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
-     else
-        new_list = my_list[:idx] + element + my_list[idx+1:]
-        print(new_list)
-        print(my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
